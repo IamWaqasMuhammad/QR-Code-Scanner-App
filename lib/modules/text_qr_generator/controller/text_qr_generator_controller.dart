e26d171c8b9cb6301d77e261aab7592ed2441b03
IamWaqasMuhammad <waqas.developer5254@gmail.com>
2026-02-17 18:47:10 +0500

    generate qr code for text implemented

--- lib/modules/text_qr_generator/controller/text_qr_generator_controller.dart
+++ lib/modules/text_qr_generator/controller/text_qr_generator_controller.dart
@@ -1,4 +1,29 @@
-
 import '../../../app_barrels.dart';
 
-class TextQRGeneratorController extends GetxController{}
+class TextQRGeneratorController extends GetxController {
+
+  final TextEditingController textController = TextEditingController();
+
+  var qrData = ''.obs;
+
+  void generateQR() {
+    if (textController.text.trim().isEmpty) {
+      Get.snackbar(
+        'Empty Input',
+        'Please enter some text to generate QR',
+        backgroundColor: Colors.red.withValues(alpha: 0.2),
+        colorText: AppColors.whiteGrayColor,
+      );
+      return;
+    }
+
+    qrData.value = textController.text.trim();
+    FocusManager.instance.primaryFocus?.unfocus();
+  }
+
+  @override
+  void onClose() {
+    textController.dispose();
+    super.onClose();
+  }
+}
